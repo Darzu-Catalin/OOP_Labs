cacc30121c8b99b7ae5e91503781bda705659496
Darzu Catalin <cs.darzu@gmail.com>
2024-10-29 18:38:09 +0200

    task 1

---
+++ OOP/src/main/java/Lab1/Task1/Display.java
@@ -0,0 +1,68 @@
+package Lab1.Task1;
+
+public class Display {
+    private int width;
+    private int height;
+    private float ppi;
+    private String model;
+
+    public Display(int width, int height, float ppi, String model) {
+        this.width = width;
+        this.model = model;
+        this.ppi = ppi;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getPpi() {
+        return ppi;
+    }
+
+    public void setPpi(float ppi) {
+        this.ppi = ppi;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void compareSize(Display m){
+        String s = (this.height * this.width) > (m.getHeight() * m.getWidth()) ?
+                    this.model + " has a bigger size" :
+                    (this.height * this.width) == (m.getHeight() * m.getWidth()) ?
+                    this.model + " " + m.getModel() + " have the same size" :
+                    m.getModel() + " has a bigger size";
+        System.out.println(s);
+    }
+    public void compareSharpness(Display m){
+        String s = this.ppi > m.getPpi() ?
+                this.model + " is sharper" :
+                (this.ppi) == (m.getPpi()) ?
+                this.model + " " + m.getModel() + " are equally sharp" :
+                m.getModel() + " is sharper";
+        System.out.println(s);
+    }
+    public void compareWithMonitor(Display m){
+        compareSize(m);
+        compareSharpness(m);
+    }
+}
---
+++ OOP/src/main/java/Lab1/Task1/Main.java
@@ -0,0 +1,21 @@
+package Lab1.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Display d1 = new Display(200, 100, 20.5f, "ASUS");
+        Display d2 = new Display(100, 50, 40f, "LENOVO");
+        Display d3 = new Display(300, 200, 45f, "MSI");
+
+//        d1.compareSize(d2);
+//        d1.compareSharpness(d2);
+        System.out.println("Display 1 and 2 comparison");
+        d1.compareWithMonitor(d2);
+        System.out.println("\nDisplay 1 and 3 comparison");
+        d1.compareWithMonitor(d3);
+        System.out.println("\nDisplay 2 and 3 comparison");
+        d2.compareWithMonitor(d3);
+
+
+    }
+}
